0e42100d9f2303edd81604011d4618f54bbdef2f
thiagomcoffice <thiago.pivotwow@gmail.com>
2015-08-19 21:58:33 -0300

    FINISH WITH FLOAT

--- TrianguloDesafio/src/Classes/WriteText.java
+++ TrianguloDesafio/src/Classes/WriteText.java
@@ -7,16 +7,45 @@ import java.io.IOException;
 
 public class WriteText {
 
-	public static float ReturnText(String LocalFile) throws IOException {
-		long timeIni = System.currentTimeMillis(), timeFinal;
-		float resultado = 0;
-		int contLine = ReturnLengh(LocalFile);
+	public static float ReturnSoma(String LocalFile) throws IOException {
+		long timeIni = System.currentTimeMillis(), timeFinal, deltaTime;
+		int contLine = ReturnLengh(LocalFile), posResultado = 1, posC = 0;
+		float[] resultado = new float[contLine];
+		float resFinal = 0;
 
 		float[][] matriz = new float[contLine][contLine];
 
 		PopulationMatriz(LocalFile, matriz);
+		
+		while (posC < matriz.length) {
+			resultado[posC] = matriz[matriz.length - 1][posC];
+			posResultado = posC;
+			for (int i = matriz.length - 1; i > 0; i--) {
+				if (posResultado == 0 || posC == 0) {
+					resultado[posC] += matriz[i-1][0];
+				}else if(posResultado == i){
+					resultado[posC] += matriz[i-1][posResultado - 1];
+					posResultado--;
+				}else if(matriz[i-1][posResultado - 1] > matriz[i-1][posResultado]){
+					resultado[posC] += matriz[i-1][posResultado - 1];
+					posResultado--;
+				}else{
+					resultado[posC] += matriz[i-1][posResultado];
+				}
+			}
+			posC++;
+		}
+		for (int i = 0; i < matriz.length; i++) {
+			if(resultado[i] > resFinal){
+				resFinal = resultado[i];
+			}
+		}
 
-		return resultado;
+		timeFinal = System.currentTimeMillis();
+		deltaTime = (timeFinal) - (timeIni);
+
+		System.out.println("Soma = " + resFinal + "\nTime = " + deltaTime + "(ms)");
+		return resFinal;
 	}
 
 	public static int ReturnLengh(String LocalFile) throws IOException {
@@ -53,32 +82,17 @@ public class WriteText {
 			FileReader arquivo = new FileReader(LocalFile);
 			BufferedReader writeFile = new BufferedReader(arquivo);
 			line = writeFile.readLine();
-			int contStr = 0, posInicial = 0, posFinal = 0;
+			int contStr = 0;
 
 			while (line != null) {
 				if (contLine == 0) {
 					Matriz[contLine][contStr] = Float.parseFloat(line);
 				} else {
-					posInicial = 0;
-					posFinal = 0;
-					contStr = 0;
-					
-					String[] splitT = line.split(" "); 
+
+					String[] splitT = line.split(" ");
 					for (int i = 0; i < splitT.length; i++) {
 						Matriz[contLine][i] = Float.parseFloat(splitT[i]);
 					}
-					/*
-					for (int i = 0; i < line.length(); i++) {
-						if (line.charAt(i) == ' ') {
-							posFinal = i;
-							Matriz[contLine][contStr] = Float.parseFloat(line
-									.substring(posInicial, posFinal));
-							posInicial = i + 1;
-							contStr++;
-						}
-					}
-					Matriz[contLine][contStr] = Float.parseFloat(line
-							.substring(posInicial, line.length()));*/
 				}
 				line = writeFile.readLine();
 				contLine++;
